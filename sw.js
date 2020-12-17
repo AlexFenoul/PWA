a109142c5829cad47526e45b3605cdfc3643ef3d
AlexFenoul <alexandre.fenoul@ynov.com>
2020-12-17 13:08:43 +0100

    Update sw

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@ self.addEventListener('fetch', function(e) {
     e.respondWith(
         caches.match(e.request).then(function(r) {
             console.log('[Service Worker] Fetching resource: '+e.request.url);
-            return console.log('r'), r || fetch(e.request).then(function(response) {
+            return r || fetch(e.request).then(function(response) {
                 return caches.open(cacheName).then(function(cache) {
                     console.log('[Service Worker] Caching new resource: ' + e.request.url);
                     cache.put(e.request, response.clone());
